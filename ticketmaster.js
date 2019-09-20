8f0866c9b2a8430c7fa31d82bd2f0d4fb5801413
Perry Williams <perrywilliams@800appliance.com>
2019-09-20 10:33:51 -0500

    back to starting point.. trying to pull js from html, make it a function call

--- ticketmaster.js
+++ ticketmaster.js
@@ -1,29 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Project 1</title>
-<!-- Bootstrap -->
+// Performing AJAX GET request
 
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.3.1.js" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<!-- jQuery -->
+// this is a slightly different request, didn't contain venue information.
+// queryURL = "https://app.ticketmaster.com/discovery/v2/attractions.json?apikey=mTEygy6LqzguOiGcxe4nsIaFBEQTdwN3&keyword=metallica";
 
+// function getEventData(artistToSearch){
 
-<script
-  src="https://code.jquery.com/jquery-3.4.1.js"
-  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
-  crossorigin="anonymous"></script>
+console.log("getEventData(artistToSearch) functin called");
 
+queryURL = "https://app.ticketmaster.com/discovery/v2/events?apikey=mTEygy6LqzguOiGcxe4nsIaFBEQTdwN3&keyword="+artistToSearch+"&preferredCountry=us&includeSpellcheck=yes"
 
+var response = [];
+response.length = 0;
+      $.ajax({
+      url: queryURL,
+      method: "GET"
+      }).then(function (response) {
+          console.log(response);
+          document.getElementById("eventsHere").innerHTML = "";
 
-
-</head>
-<body>
-    
-</body>
-</html>
+          // Start a for loop to create giph diplay div's with onclick events to start and stop motion.
+          for (var i = 0; i < response._embedded.events.length; i++) {
+                  var divInfo = `<h2 style="margin-bottom :0px">${response._embedded.events[i].name}</h2>
+                  <div class="eventDiv" style="background-color: beige; color:blue;" >
+                  <span><img src="${response._embedded.events[i].images[1].url}" height ="50px;" alt="Cool picture dude!"></span>;
+                  <span style="vertical-align:top"> ${response._embedded.events[i].dates.start.localDate}</span><br>
+                  <span style="vertical-align:top"> ${response._embedded.events[i].info}</span><br>
+                  <span style="vertical-align:top">LOCATION: 
+                  ${response._embedded.events[i]._embedded.venues[0].name},
+                  ${response._embedded.events[i]._embedded.venues[0].city.name} 
+                  ${response._embedded.events[i]._embedded.venues[0].state.name} 
+                  ${response._embedded.events[i]._embedded.venues[0].country.name}  
+                  </span><br>
+                  <a href="${response._embedded.events[i].url}">Buy Tickets</a> 
+                  </div>
+                  `
+                  document.getElementById("eventsHere").innerHTML += divInfo;
+              }
+          });
+        // }
